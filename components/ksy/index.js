ef1c4e7d8dbfb83b90a28fc3379f4974d3b00273
ksyee <tjssud0647@gmail.com>
2023-11-19 13:55:19 +0900

    tech stack의 tab 클릭시 active기능 추가

--- components/ksy/index.js
+++ components/ksy/index.js
@@ -3,5 +3,10 @@ const tabs = document.querySelectorAll('.tech-stack .tab-bar .tab');
 console.log(tabs);
 
 tabs.forEach((tab) => {
-  tab.addEventListener('click', () => {});
+  tab.addEventListener('click', () => {
+    tabs.forEach((item) =>
+      item.querySelector('button').classList.remove('is--active')
+    );
+    tab.querySelector('button').classList.add('is--active');
+  });
 });
